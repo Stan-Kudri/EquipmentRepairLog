a0925b4b02cadbae0a191390d8600a771a0e8e6c
Stan___Kudri <53861218+Stan-Kudri@users.noreply.github.com>
2025-04-08 00:09:59 +0300

    EquipmentRepairLog.Core - Add standart class for base table.

---
+++ EquipmentRepairLog.Core/Data/Model/Division.cs
@@ -0,0 +1,9 @@
+﻿namespace EquipmentRepairLog.Core.Data.Model
+{
+    public class Division : Entity
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public int Number { get; set; }
+    }
+}
---
+++ EquipmentRepairLog.Core/Data/Model/Perfomer.cs
@@ -0,0 +1,9 @@
+﻿namespace EquipmentRepairLog.Core.Data.Model
+{
+    public class Perfomer : Entity
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public string Abbreviation { get; set; } = string.Empty;
+    }
+}
