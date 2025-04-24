76659edc53a9f5fc326384fe13a3891086ef2225
Sergey <53861218+Stan-Kudri@users.noreply.github.com>
2025-04-24 23:05:38 +0300

    ERD.Core - Add User Service (#16)

---
+++ EquipmentRepairLog.Core/Service/UserService.cs
@@ -0,0 +1,37 @@
+﻿using EquipmentRepairLog.Core.Data.User;
+using EquipmentRepairLog.Core.DBContext;
+
+namespace EquipmentRepairLog.Core.Service
+{
+    public class UserService
+    {
+        private readonly AppDbContext _dbContext;
+
+        public UserService(AppDbContext appDbContext) => _dbContext = appDbContext;
+
+        public void Add(User user)
+        {
+            if (user == null)
+            {
+                throw new ArgumentException("The received parameters are not correct.");
+            }
+
+            if (_dbContext.Users.Any(e => e.Username == user.Username))
+            {
+                throw new ArgumentException("This username exists.");
+            }
+
+            _dbContext.Users.Add(user);
+            _dbContext.SaveChanges();
+        }
+
+        public User? GetUser(string username, string passwordHash)
+            => _dbContext.Users.FirstOrDefault(e => e.Username == username && e.PasswordHash == passwordHash);
+
+        public bool IsFreeUsername(string username)
+            => _dbContext.Users.FirstOrDefault(e => e.Username == username) == null;
+
+        public bool IsUserData(User user)
+            => _dbContext.Users.FirstOrDefault(e => e.Username == user.Username && e.PasswordHash == user.PasswordHash) != null;
+    }
+}
