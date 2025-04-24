f5af8db9dc2e631e101d3683952b6f61596d72ba
Stan___Kudri <53861218+Stan-Kudri@users.noreply.github.com>
2025-04-25 00:39:18 +0300

    ERD.Core - Add Service Division

---
+++ EquipmentRepairLog.Core/Service/ServiceDivision.cs
@@ -0,0 +1,51 @@
+﻿using EquipmentRepairLog.Core.Data.StandardModel;
+using EquipmentRepairLog.Core.DBContext;
+
+namespace EquipmentRepairLog.Core.Service
+{
+    public class ServiceDivision
+    {
+        private readonly AppDbContext _dbContext;
+
+        public ServiceDivision(AppDbContext dbContext)
+            => _dbContext = dbContext;
+
+        public void Add(Division division)
+        {
+            if (division == null)
+            {
+                throw new ArgumentNullException("Transmitted data error.", nameof(division));
+            }
+            if (_dbContext.Divisions.FirstOrDefault(e => e.Abbreviation == division.Abbreviation
+                                                        || e.Name == division.Name
+                                                        || e.Number == division.Number) != null)
+            {
+                throw new ArgumentException("Data already in use.", nameof(division));
+            }
+            if (_dbContext.Divisions.FirstOrDefault(e => e.Id == division.Id) != null)
+            {
+                division.Id = GetIdDivision();
+            }
+
+            _dbContext.Divisions.Add(division);
+        }
+
+        public Division? GetDivision(Guid id)
+            => _dbContext.Divisions.FirstOrDefault(e => e.Id == id);
+
+        public Division? GetDivision(int number)
+            => _dbContext.Divisions.FirstOrDefault(e => e.Number == number);
+
+        private Guid GetIdDivision()
+        {
+            var id = Guid.NewGuid();
+
+            if (_dbContext.Divisions.FirstOrDefault(d => d.Id == id) == null)
+            {
+                return id;
+            }
+
+            return GetIdDivision();
+        }
+    }
+}
