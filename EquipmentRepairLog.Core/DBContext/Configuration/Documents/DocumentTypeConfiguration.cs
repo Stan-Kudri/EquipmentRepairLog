a8bffca3f7cbf4c3861f4850e963ba611fd3f8f0
Stan___Kudri <53861218+Stan-Kudri@users.noreply.github.com>
2025-05-27 23:40:23 +0300

    ERD.Core - Change document config DB.

--- EquipmentRepairLog.Core/DBContext/Configuration/Documents/DocumentTypeConfiguration.cs
+++ EquipmentRepairLog.Core/DBContext/Configuration/Documents/DocumentTypeConfiguration.cs
@@ -1,4 +1,4 @@
-﻿using EquipmentRepairLog.Core.Data.StandardModel;
+using EquipmentRepairLog.Core.Data.StandardModel;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
@@ -9,7 +9,9 @@ namespace EquipmentRepairLog.Core.DBContext.Configuration.Documents
         protected override void ConfigureModel(EntityTypeBuilder<DocumentType> builder)
         {
             builder.ToTable("document_type");
+            builder.HasIndex(x => x.Name).IsUnique();
             builder.Property(x => x.Name).HasColumnName("name").IsRequired().HasMaxLength(128);
+            builder.HasIndex(x => x.Abbreviation).IsUnique();
             builder.Property(x => x.Abbreviation).HasColumnName("abbreviation").IsRequired().HasMaxLength(32);
             builder.Property(x => x.IsOnlyTypeDocInRepairLog).HasColumnName("is_single_type_document").IsRequired().HasDefaultValue(false);
             builder.HasMany(x => x.Documents).WithOne(x => x.DocumentType).HasForeignKey(x => x.DocumentTypeId).IsRequired();
